787609b6ad8eccd2798fcbb0383f48f6db590615
JustAbusiness <huysanti123456@gmail.com>
2024-03-24 13:03:48 +0700

    fetch Data

--- src/components/users.table.tsx
+++ src/components/users.table.tsx
@@ -9,6 +9,12 @@ import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Popover from "react-bootstrap/Popover";
 import { useQuery } from "@tanstack/react-query";
 
+
+interface IUser {
+    id: number;
+    name: string;
+    email: string;
+}
 function UsersTable() {
   const [isOpenCreateModal, setIsOpenCreateModal] = useState<boolean>(false);
 
@@ -17,23 +23,23 @@ function UsersTable() {
 
   const [isOpenDeleteModal, setIsOpenDeleteModal] = useState<boolean>(false);
 
-  // const users = [
-  //     {
-  //         "id": 1,
-  //         "name": "Eric",
-  //         "email": "eric@gmail.com"
-  //     },
-  //     {
-  //         "id": 2,
-  //         "name": "Hỏi Dân IT",
-  //         "email": "hoidanit@gmail.com"
-  //     },
-  //     {
-  //         "id": 3,
-  //         "name": "Hỏi Dân IT",
-  //         "email": "admin@gmail.com"
-  //     }
-  // ]
+//   const users = [
+//       {
+//           "id": 1,
+//           "name": "Eric",
+//           "email": "eric@gmail.com"
+//       },
+//       {
+//           "id": 2,
+//           "name": "Hỏi Dân IT",
+//           "email": "hoidanit@gmail.com"
+//       },
+//       {
+//           "id": 3,
+//           "name": "Hỏi Dân IT",
+//           "email": "admin@gmail.com"
+//       }
+//   ]
 
   const handleEditUser = (user: any) => {
     setDataUser(user);
@@ -48,28 +54,48 @@ function UsersTable() {
   const PopoverComponent = forwardRef((props: any, ref: any) => {
     const { id } = props;
 
-    const { isPending, error, data: users } = useQuery({
-      queryKey: ["repoData"],
-      queryFn: () =>
-        fetch("http://localhost8000/users").then((res) => {throw  Error('An error has occurred')}
-        ),
+    const { isPending, error, data } = useQuery({
+      queryKey: ["fetchUsers", id],
+      queryFn: (): Promise<IUser> =>
+        fetch(`http://localhost8000/users/${id}`).then((res) => res.json()),
     });
 
-    if (isPending) return "Loading...";
-
-    if (error) return "An error has occurred: " + error.message;
-
+    const getBody = () => {
+        if (isPending){
+            return 'Loading...';
+        }
+        
+        if (error) return 'An error has occured : ' + error.message;
+        if (data) {
+            return (
+                <>
+                <div>ID = {id}</div>
+                <div>Name = {data?.name}</div>
+                <div>Email = {data?.email}</div>
+                </>
+            )
+        }
+    }
     return (
       <Popover ref={ref} {...props}>
         <Popover.Header as="h3">Detail User</Popover.Header>
         <Popover.Body>
-          <div>ID = {id}</div>
-          <div>Name = ?</div>
-          <div>Email = ?</div>
+          {getBody()}
         </Popover.Body>
       </Popover>
     );
   });
+
+
+  const { isPending, error, data: users } = useQuery({
+    queryKey: ["fetchUsers"],
+    queryFn: (): Promise<IUser> =>
+      fetch("http://localhost8000/users").then((res) => res.json()),
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
 
   return (
     <>
--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,14 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
     <App />
     <ToastContainer
       position="top-right"
