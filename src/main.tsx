76f5a1372ba752b1d1341cd93f0d106b7d90afa6
JustAbusiness <huysanti123456@gmail.com>
2024-03-24 12:21:19 +0700

    install and setup stanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,16 @@ import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
     <ToastContainer
       position="top-right"
@@ -20,5 +27,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       pauseOnHover
       theme="light"
     />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
